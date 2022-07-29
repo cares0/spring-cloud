7ebbbf17863204b14541ab18662634066f82a4da
cares0 <cares00000@gmail.com>
2022-07-29 18:53:44 +0900

    feat: 회원 등록 개발

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
+import com.example.userservice.vo.RequestUser;
+import com.example.userservice.vo.ResponseUser;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final Greeting greeting;
+    private final UserService userService;
 
     @GetMapping("/health_check")
     public String status() {
@@ -22,6 +28,18 @@ public class UserController {
     @GetMapping("/welcome")
     public String welcome() {
         return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity createUser(@RequestBody RequestUser user) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDto userDto = mapper.map(user, UserDto.class);
+
+        userService.createUser(userDto);
+
+        ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
     }
 
 }
