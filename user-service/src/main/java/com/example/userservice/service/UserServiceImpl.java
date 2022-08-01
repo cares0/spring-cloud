df1a252c0927e4828c6bfaf4f7b5fc741cc01bd9
cares0 <cares00000@gmail.com>
2022-08-01 23:01:46 +0900

    feat: ErrorDecoder 사용 예외 처리

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -5,7 +5,9 @@ import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.ParameterizedTypeReference;
@@ -24,6 +26,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
@@ -69,6 +72,10 @@ public class UserServiceImpl implements UserService {
                         new ParameterizedTypeReference<List<ResponseOrder>>() {});*/
 
         /* Using feign client */
+
+        /* Feign Exception Handling */
+
+        /* ErrorDecoder */
         List<ResponseOrder> orderList = orderServiceClient.getOrders(userId);
 
         userDto.setOrders(orderList);
